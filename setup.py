e33f4bc1004fd5ce20be261e60aeba8dc81e6cbb
Devon Powell <devonmpowell1@gmail.com>
2017-10-05 14:14:10 -0700

    Make readme and manifest

--- setup.py
+++ setup.py
@@ -2,20 +2,21 @@ from setuptools import setup, Extension
 import numpy.distutils.misc_util
 
 c_ext = Extension(
-    "psi", 
+    name = "PSI", 
     sources = ["src/PSI.c", "src/mesh.c","src/grid.c", "src/geometry.c", "src/skymap.c",
         "src/cpsi.c", "src/refine.c", "src/rtree.c", "src/fft.c", "src/beamtrace.c"],
     extra_objects = ["include/mesh.h","include/grid.h", "include/geometry.h", "include/skymap.h",
         "include/refine.h", "include/rtree.h", "include/fft.h", "include/beamtrace.h"],
-    define_macros=[('PYMODULE', None), ('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION'),
+    define_macros = [('PYMODULE', None), ('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION'),
         ('HAVE_FFTW', None)],
 )
 
 setup(
-    name = 'the-phase-sheet-intersector-beta',
+    name = 'phase-space-intersector',
     version = '3.0.2',
     author = 'Devon Powell',
     author_email = 'devonmpowell1@gmail.com',
+    url = '',
     ext_modules=[c_ext],
     include_dirs=['include']+numpy.distutils.misc_util.get_numpy_include_dirs(),
 )
