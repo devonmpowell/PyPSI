43673ea9ad0f45d4ce6b5916c092c04d377e376b
Devon Powell <devonmpowell1@gmail.com>
2023-07-21 21:38:03 +0200

    Clean up setupy.py

--- setup.py
+++ setup.py
@@ -1,7 +1,3 @@
-# for python 2.x compatibility
-from __future__ import print_function
-if hasattr(__builtins__, 'raw_input'):
-    input = raw_input
 from setuptools import setup, Extension
 import numpy as np
 import sys
@@ -9,8 +5,8 @@ import os
 
 
 # default build behavior
-srcfiles = ["src/PSI.c", "src/mesh.c","src/grid.c", "src/geometry.c",
-        "src/cpsi.c", "src/refine.c", "src/rtree.c",] # "src/beamtrace.c", "src/skymap.c",]
+srcfiles = ["src/PSI.c", "src/mesh.c", "src/grid.c", "src/geometry.c",
+        "src/cpsi.c", "src/refine.c", "src/rtree.c",]
 libs = []
 libdirs = []
 macros = [('PYMODULE', None), ('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION'),]
@@ -23,15 +19,16 @@ c_ext = Extension(
     define_macros = macros,
     library_dirs = libdirs,
     libraries = libs,
+    extra_compile_args = ["-w"],
 )
 
 # build the extension
 setup(
     name = 'the-phase-space-intersector',
-    version = '3.0.2',
+    version = '3.1.2',
     author = 'Devon Powell',
     author_email = 'devonmpowell1@gmail.com',
-    url = 'https://upload.pypi.org/legacy/',
+    url = 'https://github.com/devonmpowell/PyPSI',
     ext_modules=[c_ext],
     install_requires = ['numpy'],
     include_dirs=['include', np.get_include()],
